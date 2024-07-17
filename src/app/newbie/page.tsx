2a047b589a5ba1ab21af74e6136143f15d28a680
prahul25 <palrahulkumar2002@gmail.com>
2024-07-18 00:42:24 +0530

    creation of application

---
+++ src/app/newbie/page.tsx
@@ -0,0 +1,3 @@
+export default function Newbie(){
+    return <h1 className="bg-violet-300 text-black text-2xl text-center">From Newbie</h1>
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="bg-blue-300 text-black text-2xl text-center">From Home</h1>
+  );
+}
