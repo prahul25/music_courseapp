94479b8950f7129498f4f8127a589b6681f0e871
prahul25 <palrahulkumar2002@gmail.com>
2024-07-20 17:15:28 +0530

    completed contact-us section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+'use client'
+import { Boxes } from '@/components/ui/background-boxes'
+import React, { FormEvent } from 'react'
 
-function page() {
+function ContactPage() {
+  const [email,setEmail] = React.useState("")
+  const [text,setText] = React.useState("")
+  function submitHandler(event:FormEvent) {
+    event.preventDefault()
+    setEmail("")
+    setText("")
+    alert('Successfully form submitted')
+  }
   return (
-    <div>
-      
+    <div className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        
+      <Boxes/>
+      <div className='flex justify-center flex-col items-center'>
+          <h1 className='text-lg md:text-6xl text-white font-bold relative z-20'>Contact Us</h1>
+          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
+          We&apos;re here to help with any questions about our courses,
+          programs, or events. Reach out and let us know how we can assist you
+          in your musical journey.
+        </p>
+        <form onSubmit={submitHandler} className='relative z-20'>
+          <label htmlFor="mail" className='mt-10 mb-20'>Email</label>
+          <input type="email" name="mail" id="mail" value={email} onChange={(e) =>(setEmail(e.target.value))} placeholder="Your email address"
+            className="rounded-lg border border-neutral-800 my-2 dark:border-[#DC143C] focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+            required/>
+          <label htmlFor="textTag">Message</label>
+          <textarea name="textTag" id="textTag" value={text} onChange={(e) =>(setText(e.target.value))} placeholder="Your message"
+            className="rounded-lg border border-neutral-800 my-2 dark:border-[#DC143C] focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+            rows={5}
+            required></textarea>
+          <button type="submit" className='mt-4 bg-[#DC143C] hover:bg-[#a31232] text-white font-bold py-2 px-4 rounded-lg focus:bg-teal-500 transition-colors duration-300'>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default page
+export default ContactPage
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from "next/image";
-import Link from "next/link";
 import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card';
 import courseData from "@/data/music_courses.json"
 
@@ -23,17 +22,17 @@ function page() {
       <div className='flex flex-wrap justify-center'>
         {courseData.courses.map((course)=>(
           <CardContainer key={course.id} className='inter-var m-6'>
-            <CardBody  className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+            <CardBody  className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-[#800000] dark:border-[#DC143C] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
             <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-xl font-bold text-neutral-600 dark:text-[#F7E7CE]"
         >
           {course.title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          className="text-[#C9C9C9] text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
         {course.description}
         </CardItem>
